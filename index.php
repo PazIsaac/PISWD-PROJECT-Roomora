1bbee683078bd9571287950173c677fa76907245
Alexis-martine <alexismartinez13am6494007@gmail.com>
2026-05-22 15:34:21 -0300

    inicio de pagina

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+$titulo = 'Roomora';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title><?php echo $titulo; ?></title>
+</head>
+<body>
+    <!-- <div class="contactos">
+        <h1>Bienvenidos a <?php echo $titulo; ?></h1>
+        <h2>contactos👇</h2>
+        <a href="1125797652"></a>
+        <a href="1130361637"></a>
+    </div>
+    <div class="empleados">
+        <a href="esclavo(aaron)"></a>
+        <a href="alexis"></a>
+    </div>-->
+    <header>
+        <nav>
+            <ul>
+                <li><a class="inicio" href="inicio.php">Inicio</a></li>
+                <li><a class="Registro" href="Registro.php">Registro</a></li>
+                <li><a class="Depa" href="Depa.php">Departamento</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Bienvenidos a <?php echo $titulo; ?></h1>
+        <p>En <?php echo $titulo; ?>, nos dedicamos a ofrecer las mejores soluciones para tus necesidades. Nuestro equipo de expertos está comprometido en brindarte un servicio excepcional y productos de alta calidad.</p>
+        <p>Explora nuestro sitio para descubrir más sobre nuestros servicios y cómo podemos ayudarte a alcanzar tus objetivos.</p>
+    </main>
+
+    <footer>
+        
+    </footer>
+
+</body>
+</html>
